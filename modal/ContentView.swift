be35b76d1c683d2974ccc364349dcae6696950c6
maeganwilson_ <maegan@maeganwilson.com>
2019-08-18 16:24:33 -0500

    Initial Commit

---
+++ modal/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  modal
+//
+//  Created by Maegan Wilson on 8/18/19.
+//  Copyright © 2019 MW. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
